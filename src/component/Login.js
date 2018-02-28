c0820b2c4614d53f33967872258b77e1a1f2ea1c
sahilExcellenceTech <sahil@excellencetechnologies.in>
2018-02-28 10:36:59 +0530

    first commit

---
+++ src/component/Login.js
@@ -0,0 +1,68 @@
+
+import React from 'react';
+import { Form,Col, Input, Button, notification } from 'antd';
+import {Link} from 'react-router-dom';
+
+const FormItem = Form.Item;
+
+class NormalLoginForm extends React.Component {
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.form.validateFields((err, values) => {
+      if (!err) {
+        this.props.history.push('/homepage');
+
+        notification.open({
+          message: 'Successfully Logged In',
+          description: 'You are successfully Logged in.',
+        });
+      }
+    });
+  }
+  render() {
+    const { getFieldDecorator } = this.props.form;
+    return (
+    <div id="container">
+      <div id="content">
+      <h1>Login</h1>
+
+      <Form onSubmit={this.handleSubmit} className="login-form">
+      <FormItem >
+      E-Mail Address*
+        {getFieldDecorator('email', {
+          rules: [{
+            type: 'email', message: 'The input is not valid E-mail!',
+          }, {
+            required: true, message: 'Please input your E-mail!',
+          }],
+        })(
+          <Input type="text" />
+        )}
+      </FormItem>
+        <FormItem>
+        Password*
+          {getFieldDecorator('password', {
+            rules: [{ required: true, message: 'Please input your Password!' }],
+          })(
+            <Input  type="password" />
+          )}
+        </FormItem>
+        <FormItem>
+        <Button htmlType="submit" id="login-form-button" className="login-form-button">
+          Login
+        </Button>
+          <div id="links">
+          <Link to="/forgetpassword">Forgot password? Reset it!</Link><br />
+          <Link  to="/register">No Account yet? Register now!</Link>
+          </div>
+        </FormItem>
+      </Form>
+      </div>
+      </div>
+    );
+  }
+  }
+
+const WrappedNormalLoginForm = Form.create()(NormalLoginForm);
+
+export default WrappedNormalLoginForm;
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import ReactDOM from 'react-dom';
+import style from 'antd/dist/antd.css';
+import styles from './index.css';
+import styl from './HomePage.css';
+import routes from './App';
+
+ReactDOM.render(routes, document.getElementById('root'));
